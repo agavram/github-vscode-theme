e33fafcb48043e4201f72edddb157c9e374d6c9d
Adrian Avram <Adrian_Avram@Outlook.com>
2021-08-17 20:17:23 -0700

    Update colors

--- src/colors.js
+++ src/colors.js
@@ -3,6 +3,10 @@ const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
 const dimmedColors = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
 
 function getColors(theme) {
+  lightColors.bg.canvasInset = '#ffffff'
+  dimmedColors.bg.canvasInset = '#22272E'
+  darkColors.bg.canvasInset = '#0D1117'
+
   if (theme === "light") {
     return lightColors;
   } else if (theme === "dark") {
